aa2f218a1ac55772aedaa5e6a18e5182592bcc4c
David G Chaves <davidgchaves@gmail.com>
2015-03-03 10:01:21 +0100

    Add action for new contact
    It doesn't compile yet because we need to define:
      - The Contact model.
      - The newContact template.

---
+++ app/controllers/Contacts.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+import play.data.Form;
+
+public class Contacts extends Controller{
+    public static Result newContact() {
+        Form<Contact> contactForm = Form.form(Contact.class);
+        return ok(views.html.contacts.newContact.render(contactForm));
+    }
+}
