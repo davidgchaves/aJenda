be39ef177df6d0f28f759e29ee9757e9cd72eef9
David G Chaves <davidgchaves@gmail.com>
2015-03-05 18:09:32 +0100

    Add constructor to Contact
    Needed to compile 'Show contact details' scenario

--- app/models/Contact.java
+++ app/models/Contact.java
@@ -18,5 +18,11 @@ public class Contact extends Model {
   public String phone;
   public String email;
 
+  public Contact(String name, String phone, String email) {
+    this.name = name;
+    this.phone = phone;
+    this.email = email;
+  }
+
   public static Finder<Long, Contact> finder = new Finder<Long, Contact>(Long.class, Contact.class);
 }
